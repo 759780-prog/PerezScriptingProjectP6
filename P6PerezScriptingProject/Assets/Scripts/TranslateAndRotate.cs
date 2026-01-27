9ba039f665152d368215e8b023dcb5609b84eeb9
759780-prog <759780@student.dusd.net>
2026-01-27 13:31:57 -0800

    translate and rotate
    finished translate and rotate

---
+++ P6PerezScriptingProject/Assets/Scripts/TranslateAndRotate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformFunctions : MonoBehaviour
+{
+    public float moveSpeed = 999999999999f;
+    public float turnSpeed = 999999999999f;
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+    }
+}
